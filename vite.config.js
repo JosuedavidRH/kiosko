0a24548debf0f8f22dc7f3eea5227db22bb514ac
JosuedavidRH <jrh24555@gmail.com>
2025-09-26 17:33:28 -0500

    v_112

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'], // soporta navegadores antiguos
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'], // para async/await
-    }),
   ],
   build: {
     sourcemap: true, // 👈 esto habilita los source maps
